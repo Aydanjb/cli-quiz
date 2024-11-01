de662056f2e8cb6bb99ea1f48c596a35a6fcb654
Aydanjb <AydanBondurant@gmail.com>
2024-10-31 20:56:20 -0400

    added csv parsing

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatalf("Failed to open file: %s\n", file.Name())
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	problems, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatalf("Failed to parse file: %s\n", file.Name())
+	}
+
+	fmt.Println(problems)
+
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s\n", i+1, problem[0])
+		fmt.Printf("Answer: %s\n", problem[1])
+	}
 }
